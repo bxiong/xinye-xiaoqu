638a218ffd3b42492ebfca9307bba30b785b9c73
dsdjm <dsdjm2013@163.com>
2013-11-02 15:21:45 +0800

    add phone info

--- server/src/main/java/net/xiaoqu/weixin/WeiXinController.java
+++ server/src/main/java/net/xiaoqu/weixin/WeiXinController.java
@@ -24,20 +24,17 @@ import java.util.ResourceBundle;
 @Controller
 @RequestMapping("/jlgj/")
 public class WeiXinController {
-//    @Autowired
-//    org.springframework.context.support.ResourceBundleMessageSource resourceBundleMessageSource;
+
     private final String TOKEN = "Jlgj20131021";
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     @ResponseBody
     public String get(HttpServletRequest request, ModelMap model) {
-        // System.out.println("xiongbo get");
+
         String signature = request.getParameter("signature");
         String timestamp = request.getParameter("timestamp");
         String nonce = request.getParameter("nonce");
         String echostr = request.getParameter("echostr");
-
-        // System.out.println("xiongbo signature : " + signature + " timestamp : " + timestamp + " nonce : " + nonce + " echostr : " + echostr);
 
         if (null != timestamp && null != nonce && null != signature) {
             if (WeiXin.access(TOKEN, signature, timestamp, nonce)) {
@@ -57,10 +54,8 @@ public class WeiXinController {
     public String post(HttpServletRequest request, ModelMap model) {
         Locale locale = RequestContextUtils.getLocaleResolver(request).resolveLocale(request);
 
-        ResourceBundle myResources =
-                ResourceBundle.getBundle("messages", locale);
-//        String vaule = resourceBundleMessageSource.getMessage("welcome",null, locale);
-        String value= myResources.getString("welcome");
+        ResourceBundle r = ResourceBundle.getBundle("messages", locale);
+
 
         System.out.println("xiongbo post");
         String signature = request.getParameter("signature");
@@ -68,7 +63,7 @@ public class WeiXinController {
         String nonce = request.getParameter("nonce");
         String echostr = request.getParameter("echostr");
 
-        System.out.println("xiongbo signature : " + signature + " timestamp : " + timestamp + " nonce : " + nonce + " echostr : " + echostr);
+//        System.out.println("xiongbo signature : " + signature + " timestamp : " + timestamp + " nonce : " + nonce + " echostr : " + echostr);
 
         if (null != timestamp && null != nonce && null != signature) {
             if (WeiXin.access(TOKEN, signature, timestamp, nonce)) {
@@ -86,11 +81,13 @@ public class WeiXinController {
                         String event = m.getEvent();
                         // 有人关注微信帐号
                         if ("subscribe".equals(event)) {
-                            String content = "欢迎关注xxx";
+                            String content = r.getString("welcome_icon") + r.getString("splittor") + r.getString("thanks_text") + r.getString("splittor")
+                                    + r.getString("splittor") + r.getString("phone_text") + r.getString("splittor") + r.getString("add_phone_title");
+                            ;
                             sendMsg = new WxSendTextMsg(sendMsg, content);
                             ByteArrayOutputStream os = new ByteArrayOutputStream();
                             WeiXin.send(sendMsg, os);
-                            return new String(os.toByteArray(), Charset.forName("iso-8859-1"));
+                            return new String(os.toByteArray(), Charset.forName("utf-8"));
                         } else if ("unsubscribe".equals(event)) {
 
                         }
@@ -99,17 +96,14 @@ public class WeiXinController {
                     // 消息推送，文本/图片/地理位置/链接/
                     if (msg instanceof WxRecvTextMsg) {
                         WxRecvTextMsg recvTextMsg = (WxRecvTextMsg) msg;
-                        String content = "（●ω●） 欢迎关注xxx'111' 15800488118/r/n\r\n 18616533721"+value;
+                        String content = r.getString("welcome_icon") + r.getString("splittor") + r.getString("phone_title") + r.getString("splittor")
+                                + r.getString("splittor") + r.getString("phone_text") + r.getString("splittor") + r.getString("add_phone_title");
                         // 构建文本消息进行发送
                         sendMsg = new WxSendTextMsg(sendMsg, content);
-//                        WxSendNewsMsg newsMsg=new WxSendNewsMsg(sendMsg);
-//                        newsMsg.addItem("title","description","https://mp.weixin.qq.com/mpres/htmledition/images/pic_myapp_func_02_zh_CN.jpg"
-//                        ,"http://115.29.175.143/xiaoqu/jlgj/index.do");
-
                         // 发送回微信
                         ByteArrayOutputStream os = new ByteArrayOutputStream();
                         WeiXin.send(sendMsg, os);
-                        return new String(os.toByteArray(), Charset.forName("iso-8859-1"));
+                        return new String(os.toByteArray(), Charset.forName("utf-8"));
                     }
                 } catch (IOException ioe) {
                     System.out.println("xiongbo ioe xception");
