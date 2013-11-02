1b87b9b80b4d4e1326cdb175cef13757ea276912
dsdjm <dsdjm2013@163.com>
2013-11-02 14:13:44 +0800

    support i18n

--- server/src/main/java/net/xiaoqu/weixin/WeiXinController.java
+++ server/src/main/java/net/xiaoqu/weixin/WeiXinController.java
@@ -12,36 +12,39 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 @Controller
 @RequestMapping("/jlgj/")
 public class WeiXinController {
+//    @Autowired
+//    org.springframework.context.support.ResourceBundleMessageSource resourceBundleMessageSource;
     private final String TOKEN = "Jlgj20131021";
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     @ResponseBody
     public String get(HttpServletRequest request, ModelMap model) {
-        System.out.println("xiongbo get");
+        // System.out.println("xiongbo get");
         String signature = request.getParameter("signature");
         String timestamp = request.getParameter("timestamp");
         String nonce = request.getParameter("nonce");
         String echostr = request.getParameter("echostr");
 
-        System.out.println("xiongbo signature : " + signature + " timestamp : " + timestamp + " nonce : " + nonce + " echostr : " + echostr);
+        // System.out.println("xiongbo signature : " + signature + " timestamp : " + timestamp + " nonce : " + nonce + " echostr : " + echostr);
 
         if (null != timestamp && null != nonce && null != signature) {
             if (WeiXin.access(TOKEN, signature, timestamp, nonce)) {
-                System.out.println("xiongbo success");
-                //TODO 校验通过
+                // 校验通过
                 return echostr;
             } else {
-                System.out.println("xiongbo failed");
-                // TODO 校验失败
+                // 校验失败
             }
             return "get";
         }
@@ -52,6 +55,13 @@ public class WeiXinController {
     @RequestMapping(value = "/index", method = RequestMethod.POST)
     @ResponseBody
     public String post(HttpServletRequest request, ModelMap model) {
+        Locale locale = RequestContextUtils.getLocaleResolver(request).resolveLocale(request);
+
+        ResourceBundle myResources =
+                ResourceBundle.getBundle("messages", locale);
+//        String vaule = resourceBundleMessageSource.getMessage("welcome",null, locale);
+        String value= myResources.getString("welcome");
+
         System.out.println("xiongbo post");
         String signature = request.getParameter("signature");
         String timestamp = request.getParameter("timestamp");
@@ -63,55 +73,52 @@ public class WeiXinController {
         if (null != timestamp && null != nonce && null != signature) {
             if (WeiXin.access(TOKEN, signature, timestamp, nonce)) {
                 System.out.println("xiongbo success");
-                //TODO 校验通过
+                //校验通过
                 try {
                     WxRecvMsg msg = WeiXin.recv(request.getInputStream());
-                    System.out.println("xiongbo msgid:"+msg.getMsgId()+" to:"+msg.getToUser()+
-                            " from:"+msg.getFromUser()+" type:"+msg.getMsgType()+" createtime:"+msg.getCreateDt());
+                    System.out.println("xiongbo msgid:" + msg.getMsgId() + " to:" + msg.getToUser() +
+                            " from:" + msg.getFromUser() + " type:" + msg.getMsgType() + " createtime:" + msg.getCreateDt());
                     WxSendMsg sendMsg = WeiXin.builderSendByRecv(msg);
 
-                    // 微信事件消息, 关注/取消关注/菜单...
-                    if(msg instanceof WxRecvEventMsg) {
+                    // 事件推送, 关注/取消关注
+                    if (msg instanceof WxRecvEventMsg) {
                         WxRecvEventMsg m = (WxRecvEventMsg) msg;
                         String event = m.getEvent();
                         // 有人关注微信帐号
-                        if("subscribe".equals(event)) {
+                        if ("subscribe".equals(event)) {
                             String content = "欢迎关注xxx";
                             sendMsg = new WxSendTextMsg(sendMsg, content);
-                            ByteArrayOutputStream os=new ByteArrayOutputStream();
+                            ByteArrayOutputStream os = new ByteArrayOutputStream();
                             WeiXin.send(sendMsg, os);
-                            // 构建文本消息进行发送
-                            // 发送回微信
-//                            WeiXin.send(sendMsg, getResponse().getOutputStream());
                             return new String(os.toByteArray(), Charset.forName("iso-8859-1"));
-                        }else if("unsubscribe".equals(event)){
+                        } else if ("unsubscribe".equals(event)) {
 
                         }
                     }
 
-                    if(msg instanceof WxRecvTextMsg){
-                        WxRecvTextMsg recvTextMsg=(WxRecvTextMsg)msg;
-                        String content = "欢迎关注xxx111";
+                    // 消息推送，文本/图片/地理位置/链接/
+                    if (msg instanceof WxRecvTextMsg) {
+                        WxRecvTextMsg recvTextMsg = (WxRecvTextMsg) msg;
+                        String content = "（●ω●） 欢迎关注xxx'111' 15800488118/r/n\r\n 18616533721"+value;
                         // 构建文本消息进行发送
                         sendMsg = new WxSendTextMsg(sendMsg, content);
+//                        WxSendNewsMsg newsMsg=new WxSendNewsMsg(sendMsg);
+//                        newsMsg.addItem("title","description","https://mp.weixin.qq.com/mpres/htmledition/images/pic_myapp_func_02_zh_CN.jpg"
+//                        ,"http://115.29.175.143/xiaoqu/jlgj/index.do");
+
                         // 发送回微信
-                        ByteArrayOutputStream os=new ByteArrayOutputStream();
+                        ByteArrayOutputStream os = new ByteArrayOutputStream();
                         WeiXin.send(sendMsg, os);
                         return new String(os.toByteArray(), Charset.forName("iso-8859-1"));
                     }
                 } catch (IOException ioe) {
                     System.out.println("xiongbo ioe xception");
                     ioe.printStackTrace();
-                }catch (JDOMException joe){
+                } catch (JDOMException joe) {
                     System.out.println("xiongbo joi xception");
                     joe.printStackTrace();
                 }
 
-
-
-
-
-//                return echostr;
             } else {
                 System.out.println("xiongbo failed");
                 // TODO 校验失败
